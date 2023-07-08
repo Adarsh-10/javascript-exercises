2606708bae5fcd52bb9a87ee7af83d7e499e5541
Adarsh Sivasubramani <adarsh05100@gmail.com>
2023-07-08 14:38:00 +0530

    finish reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = "";
+    for(let i = string.length; i > 0; i--) {
+        reverse += string.substring(i-1, i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
