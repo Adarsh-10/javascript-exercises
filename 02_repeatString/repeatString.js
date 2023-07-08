344204c56e75ebe0a46511363acf1363ff6db740
Adarsh Sivasubramani <adarsh05100@gmail.com>
2023-07-08 14:24:07 +0530

    finish repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringArg, num) {
+    let returnStatement = ""
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        returnStatement += stringArg;
+    }
+    return returnStatement;
 };
 
 // Do not edit below this line
