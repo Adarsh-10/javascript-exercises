d1ab934dc07275392fbcd0817bd8e4daf9db6cc6
Adarsh Sivasubramani <adarsh05100@gmail.com>
2023-07-10 20:07:37 +0530

    complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+    let arr = arguments[0];
+    
+    for (let i = 1; i < arguments.length; i++) {
+        let toRemove = arguments[i];
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === toRemove) {
+                arr.splice(j,1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
